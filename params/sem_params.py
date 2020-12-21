4547df005b04d12622752e9b5d3c0b4232a7438d
lxqiang95 <lxq13925731395@163.com>
2020-12-21 21:10:08 +0800

    influence初步框架

---
+++ params/sem_params.py
@@ -0,0 +1,14 @@
+# _*_ coding: UTF-8 _*_
+# @Time     : 2020/12/21 20:31
+# @Author   : LiuXiaoQiang
+# @Site     : https://github.com/qq183727918
+# @File     : sem_params.py
+# @Software : PyCharm
+
+class ParamsTest:
+    def url_influence(self):
+        """url请求头"""
+        url = 'https://gatewaypre.vevor.net/scp-inventory-service'
+
+        return url
+//
